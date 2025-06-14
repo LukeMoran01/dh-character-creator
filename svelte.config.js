95fdce5a94001863b6560ef131a0805d28d846e0
lukemoran01 <moran.luke.ed@gmail.com>
2025-06-14 10:22:34 +0100

    try github actions

--- svelte.config.js
+++ svelte.config.js
@@ -2,17 +2,17 @@ import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const dev = process.env.NODE_ENV === 'development';
-const repo = 'dh-character-creator';
 
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter({fallback: '404.html'}),
-		paths: {
-			base: dev ? '' : `/${repo}`  // GitHub Pages serves from /<repo>
+		adapter: adapter({
+			fallback: '404.html'
+		}),
+		prerender: {
+			handleHttpError: 'warn'
 		}
-	},
-
+	}
 };
 
 export default config;
